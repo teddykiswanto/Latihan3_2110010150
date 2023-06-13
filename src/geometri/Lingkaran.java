0fff844f24f42cf0f0eee117d666207783e83fbd
teddykiswanto <teddykiswanto17@gmail.com>
2023-06-13 10:08:32 +0800

    initial commit

---
+++ src/geometri/Lingkaran.java
@@ -0,0 +1,14 @@
+
+package geometri;
+
+
+public class Lingkaran {
+    double radius;
+    
+    public double luas(){
+        return 3.14*radius*radius;
+    }
+    public double keliling(){
+        return 2*3.14*radius;
+    }
+}
---
+++ src/geometri/PersegiPanjang.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometri;
+
+/**
+ *
+ * @author 62831
+ */
+public class PersegiPanjang {
+    
+}
